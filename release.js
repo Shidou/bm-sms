ed9c6b9206ecb494f43623f6976eb1285df15aff
贾喆绚 <jiazhexuan@bmqb.com>
2017-08-08 13:15:48 +0800

    test auto publish

---
+++ release.js
@@ -0,0 +1,46 @@
+const fs = import('fs');
+const execSync = import('child_process').execSync;
+
+function cmd(command) {
+  try {
+    const result = execSync(command);
+    return result.toString()
+  } catch (error) {
+    throw(error);
+  }
+}
+
+function getVersion() {
+  try {
+    let tag = cmd('git describe --exact-match --tags $(git rev-parse HEAD)');
+    console.log('tag');
+    console.log(tag);
+    tag = tag.replace(/\r?\n|\r/g, '');
+    if (/^\d+.\d+.\d+/.test(tag)) {
+      return tag;
+    }
+    return null;
+  } catch (e) {
+    return null;
+  }
+}
+
+console.log('start release prepare');
+
+const packageInfo = JSON.parse(fs.readFileSync('package.json'));
+delete packageInfo.scripts;
+
+console.log('start release build');
+cmd('npm run build');
+
+packageInfo.main = 'lib/index.js';
+
+const version = getVersion();
+
+if (version) {
+  console.log('update release version');
+  packageInfo.version = version;
+}
+
+fs.writeFileSync('package.json', JSON.stringify(packageInfo, null, 2));
+console.log('finish release prepare');
